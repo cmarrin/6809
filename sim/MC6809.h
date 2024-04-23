661bfe85c32b8d7988360d6688af11e8e056a724
Chris Marrin <chris@marrin.com>
2024-04-23 09:58:19 -0700

    Started working on Simulator
    Still have compile errors. Also added code from sbc09 project

---
+++ sim/MC6809.h
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+    This source file is a part of the MC6809 Simulator
+    For the latest info, see http:www.marrin.org/
+    Copyright (c) 2018-2024, Chris Marrin
+    All rights reserved.
+    Use of this source code is governed by the MIT license that can be
+    found in the LICENSE file.
+-------------------------------------------------------------------------*/
+//
+//  MC6809.h
+//  6809 simulator
+//
+//  Created by Chris Marrin on 4/22/24.
+//
+
+#pragma once
+
+#include <cstdint>
+
