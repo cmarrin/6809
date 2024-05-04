c00587915472420ee9c3cbe7658324f1db119fa6
Chris Marrin <cmarrin@apple.com>
2024-05-04 16:37:53 -0700

    Added start of BOSS9 system code

---
+++ emulator/BOSS9/core.cpp
@@ -0,0 +1,8 @@
+//
+//  core.cpp
+//  as9
+//
+//  Created by Chris Marrin on 5/4/24.
+//
+
+#include "core.hpp"
---
+++ emulator/BOSS9/core.h
@@ -0,0 +1,13 @@
+//
+//  core.hpp
+//  as9
+//
+//  Created by Chris Marrin on 5/4/24.
+//
+
+#ifndef core_hpp
+#define core_hpp
+
+#include <stdio.h>
+
+#endif /* core_hpp */
