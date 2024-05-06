3328499536ccd5732f4b129f98738a0dc3c50936
Chris Marrin <cmarrin@apple.com>
2024-05-06 11:36:10 -0700

    Added a loop count to HelloWorld so it only prints 10 times

--- test/HelloWorld.asm
+++ test/HelloWorld.asm
@@ -2,10 +2,13 @@
 *  display text on terminal using UART
 
     include BOSS9.inc
-	
-	org $200
 
-main    nop
+NumPrints equ 10
+
+        org $200
+
+main    lda #NumPrints
+        sta count
 
 loop
         ldx #text3
@@ -14,10 +17,15 @@ loop
         lda #newline
         jsr putc
 
-        bra loop
+        dec count
+        bgt loop
+        
+done    bra done
 	
 text3   fcc "Hello from 6809 kit"
         fcb 0
+
+count   rmb 1
 
 	end main
 
