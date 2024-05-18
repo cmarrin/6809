06f5bb9fed8c817d1a3cdcbf7b6a463444d61035
Chris Marrin <cmarrin@apple.com>
2024-05-18 10:18:27 -0700

    Got rid of blanks from HelloWorld.asm to make it more compact

--- test/HelloWorld.asm
+++ test/HelloWorld.asm
@@ -4,22 +4,17 @@
     include BOSS9.inc
 
 NumPrints equ 10
-
         org $200
 
 main    lda #NumPrints
         sta count
-
 loop
         ldx #text3
         jsr puts
-        
         lda #newline
         jsr putc
-
         dec count
         bgt loop
-        
 done    bra done
 	
 text3   fcn "Hello from 6809 kit"
